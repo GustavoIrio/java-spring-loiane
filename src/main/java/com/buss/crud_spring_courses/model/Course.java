45c4d7d6557baf27daefc81f583caeeb981d3df1
Gustavo Buss <gustavoirio@hotmail.com>
2023-10-30 10:25:25 -0300

    Soft delete

--- src/main/java/com/buss/crud_spring_courses/model/Course.java
+++ src/main/java/com/buss/crud_spring_courses/model/Course.java
@@ -5,10 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
+@SQLDelete(sql = "UPDATE Course SET status = 'Inactive' WHERE id = ?") //soft delete - utiliza o metodo delete porem altera apenas o status ao inves de deletar
+@Where(clause = "status = 'Active'") //como usamos o soft delete, ao utilizar os get, vai puxar apenas os ativos
 public class Course {
 
     @Id
@@ -26,4 +30,10 @@ public class Course {
     @Pattern(regexp = "Back-end|Front-end")
     @Column(length = 12, nullable = false)
     private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Active|Inactive")
+    @Column(length = 12, nullable = false)
+    private String status = "Active";
 }

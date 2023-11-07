281a27c133b0681e4b05cb5f6b51b4c2499a0573
Gustavo Buss <gustavoirio@hotmail.com>
2023-11-06 23:03:21 -0300

    Creating Enums and enums converters

--- src/main/java/com/buss/crud_spring_courses/dtos/CourseDTO.java
+++ src/main/java/com/buss/crud_spring_courses/dtos/CourseDTO.java
@@ -2,10 +2,11 @@ package com.buss.crud_spring_courses.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.Length;
 
 public record CourseDTO(
         Long id,
         @NotBlank @NotNull @Length(min = 5, max = 100) String name,
-        String category) {
+        @NotBlank @NotNull @Pattern(regexp = "Front-end|Back-end") String category) {
 }
